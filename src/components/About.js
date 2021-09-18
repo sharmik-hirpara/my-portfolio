889090b5945418eeae5c84883513fcf52737417f
Sharmik Hirpara <sharmik.hirpara@gmail.com>
2021-09-18 16:52:07 +1000

    Contact me button fix

--- src/components/About.js
+++ src/components/About.js
@@ -14,9 +14,11 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center ">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Sharmik <IoVolumeHigh className="w-5 inline-block mt-2 text-gray-400 hover:text-white" onClick={() => play()}/>
-            <br className="hidden lg:inline-block" />I love to play with programs.
+            Hi, I'm Sharmik <IoVolumeHigh className="w-5 inline-block mt-2 text-gray-400 hover:text-white" size={16} onClick={() => play()}/>
           </h1>
+          <h2 className="title-font sm:text-2xl text-xl mb-4 font-medium text-white">
+            I love to play with programs.
+          </h2>
           <p className="mb-8 leading-relaxed text-gray-300">
             I am disciplined, enthusiastic and quick learner <strong> Software Developer </strong> with 3-months work experience as a full stack developer.
           </p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -110,11 +110,13 @@ export default function Contact() {
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
-          </button>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="flex-inline text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg display: inline-block">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </section>
